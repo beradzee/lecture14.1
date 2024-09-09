fc89d13a22718f31ef7b1a923e32ad239ddc5c67
beradzee <116104845+beradzee@users.noreply.github.com>
2024-09-09 21:32:59 +0400

    kaia

--- script5.js
+++ script5.js
@@ -1,1 +1,17 @@
 const numbers = [1, 2, 3, 4, 5];
+
+const score = [10, 20, 30, 40, 50, 20];
+
+const hightscore = score.filter((score) => score > 30);
+console.log(hightscore);
+
+const squad = [
+  { name: "Giorgi", isHero: true },
+  { name: "Beqa", isHero: true },
+  { name: "gio", isHero: false },
+  { name: "Levani", isHero: false },
+];
+
+const heroes = squad.filter((hero) => hero.isHero);
+
+console.log(heroes);
